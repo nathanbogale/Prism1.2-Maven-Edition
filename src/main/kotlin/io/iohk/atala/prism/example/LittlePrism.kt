8962d543df6e31c7c3087b8eb710a29e9a339462
Nathan Bogale <nathanbogale@gmail.com>
2021-11-03 01:33:46 +0300

    Update LittlePrism.kt

--- src/main/kotlin/io/iohk/atala/prism/example/LittlePrism.kt
+++ src/main/kotlin/io/iohk/atala/prism/example/LittlePrism.kt
@@ -1,71 +1,51 @@
 package io.iohk.atala.prism.example
 
-import java.sql.DriverManager.println
-import io.iohk.atala.prism.api.*
-import io.iohk.atala.prism.common.PrismSdkInternal
-import io.iohk.atala.prism.crypto.*
-import io.iohk.atala.prism.identity.*
-//import io.iohk.atala.prism.identity.DID
 import io.iohk.atala.prism.credentials.*
-import io.iohk.atala.prism.credentials.content.*
-import io.iohk.atala.prism.credentials.json.*
-import kotlinx.datetime.*
-
-
-/*
-import io.iohk.protos.CredentialBatchData
-import io.iohk.protos.GrpcOptions
-import io.iohk.atala.protos.CredentialBatchData
-import io.iohk.atala.protos.GrpcOptions
-import io.iohk.atala.prism.protos.CredentialBatchData
-import io.iohk.atala.prism.protos.GrpcOptions
-import pbandk.ByteArr
-*/
-
+import io.iohk.atala.prism.credentials.content.CredentialContent
+import io.iohk.atala.prism.credentials.json.JsonBasedCredential
+import io.iohk.atala.prism.crypto.EC
+import io.iohk.atala.prism.crypto.MerkleInclusionProof
+import io.iohk.atala.prism.crypto.derivation.KeyDerivation
+import io.iohk.atala.prism.crypto.Hash
+import io.iohk.atala.prism.crypto.derivation.KeyType
+import io.iohk.atala.prism.extras.ProtoClientUtils
+import io.iohk.atala.prism.extras.ProtoUtils
+import io.iohk.atala.prism.extras.RequestUtils
+import io.iohk.atala.prism.identity.DID
+import io.iohk.atala.prism.identity.DID.Companion.issuingKeyId
+import io.iohk.atala.prism.identity.DID.Companion.masterKeyId
+import io.iohk.atala.prism.identity.KeyInformation
+import io.iohk.atala.prism.identity.util.ECProtoOps
+import io.iohk.atala.prism.protos.*
+import kotlinx.coroutines.*
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
-
+import pbandk.decodeFromByteArray
+import pbandk.encodeToByteArray
 
 
 object LittlePrism {
-
 
     fun run() {
         println("""Hello im Little Prism""")
         println("""-This is where we start-""")
 
-
-//DID Creation
-
+        val masterKeyPair = EC.generateKeyPair()
+        val did = DID.createUnpublishedDID(masterKeyPair.publicKey)
 /*
-    val masterKeyPair = EC.generateKeyPair()
-        //val did = DID.createUnpublishedDID(masterKeyPair.publicKey)
-        val did = PrismDid.buildCanonicalFromMasterPublicKey(masterKeyPair.publicKey)
-        println("""THIS IS THE DID GENERATED: $did""" )
+// DID CREATION 1
+        val masterKeyPair = EC.generateKeyPair()
+        val did = DID.createUnpublishedDID(masterKeyPair.publicKey)
 
-
-        val credentialContent = CredentialContent(
-            JsonObject(
-                mapOf(
-                    Pair("issuerDid", JsonPrimitive(did.value)),
-                    Pair("issuanceKeyId", JsonPrimitive("Issuance-0")),
-                    Pair("credentialSubject", JsonObject(
-                        mapOf(
-                            Pair("name", JsonPrimitive("José López Portillo")),
-                            Pair("certificate", JsonPrimitive("Certificate of PRISM SDK tutorial completion"))
-                        )
-                    )),
-                )
-            )
-        )
+// DID CREATION 2
+        val mnemonic = KeyDerivation.randomMnemonicCode()
+        val seed = KeyDerivation.binarySeed(mnemonic, "secret")
+        // Create KeyPair out of mnemonic seed phrase, did index, type of key, key index
+        val issuerMasterKeyPair = DID.deriveKeyFromFullPath(seed, 0, KeyType.MASTER_KEY, 0)
+        val createDIDContext = DID.createDIDFromMnemonic(mnemonic, 0, "secret")
+        val issuerCreatedDIDSignedOperation = createDIDContext.createDIDSignedOperation
 
 */
-/*
-        val credential = JsonBasedCredential(credentialContent)
-        val signedCredential = credential.sign(masterKeyPair.privateKey)
-        val (merkleRoot, merkleProofs) = CredentialBatches.batch(listOf(signedCredential))
-*/
-
     }
 
 }

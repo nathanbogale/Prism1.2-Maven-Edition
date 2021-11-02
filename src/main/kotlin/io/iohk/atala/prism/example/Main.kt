3fedaa3dc2af290c57fac4372622e359e6de24af
Nathan Bogale <nathanbogale@gmail.com>
2021-11-03 01:34:11 +0300

    Update Main.kt

--- src/main/kotlin/io/iohk/atala/prism/example/Main.kt
+++ src/main/kotlin/io/iohk/atala/prism/example/Main.kt
@@ -1,16 +1,8 @@
 package io.iohk.atala.prism.example
 
-import java.sql.DriverManager.println
-import io.iohk.atala.prism.api.*
-import io.iohk.atala.prism.crypto.*
-import io.iohk.atala.prism.identity.*
-import io.iohk.atala.prism.credentials.*
-import io.iohk.atala.prism.credentials.content.*
-import io.iohk.atala.prism.credentials.json.*
-import kotlinx.datetime.*
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.JsonPrimitive
 
 fun main() {
+   /* NodeFlowExample.run()
+    CompleteFlowTutorial.run()*/
     LittlePrism.run()
 }

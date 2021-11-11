d9740f82511d0f973629f876cf4073f4d97aab61
Nathan Bogale <nathanbogale@gmail.com>
2021-11-12 00:36:22 +0300

    Structured JSON file

--- src/main/kotlin/io/iohk/atala/prism/example/PrismRunner2.kt
+++ src/main/kotlin/io/iohk/atala/prism/example/PrismRunner2.kt
@@ -1,15 +1,32 @@
 package io.iohk.atala.prism.example
 
+
+import java.io.File
+import io.iohk.atala.prism.api.CredentialBatches
+import io.iohk.atala.prism.credentials.*
+import io.iohk.atala.prism.credentials.content.CredentialContent
+import io.iohk.atala.prism.credentials.json.JsonBasedCredential
+import io.iohk.atala.prism.crypto.EC
+import io.iohk.atala.prism.crypto.MerkleInclusionProof
+import io.iohk.atala.prism.crypto.derivation.KeyDerivation
+import io.iohk.atala.prism.identity.PrismDid
+import io.iohk.atala.prism.protos.*
+import kotlinx.coroutines.*
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import pbandk.decodeFromByteArray
+import pbandk.encodeToByteArray
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.google.gson.annotations.SerializedName
-import io.iohk.atala.prism.crypto.EC
-import io.iohk.atala.prism.identity.PrismDid
+import org.json.JSONObject
 import org.junit.Assert.assertEquals
-import java.io.File
+
 
 
 object PrismRunner2 {
@@ -45,11 +62,38 @@ object PrismRunner2 {
                 listOf(Credential("$cred_title", "$Issuer_authority","$Category", "$Issued_Date"))
             )
         )
-        val serialized = Gson().toJson(identities)
 
-        File("result.json").writeText("$serialized")
 
-        println("""- json:. $serialized""")
+
+        val gson = Gson()
+
+
+        val joined = Json.encodeToJsonElement("$identities")
+
+        val ActualCredentialContent = CredentialContent(
+            JsonObject(mapOf(Pair("Rsponse", joined)))
+        )
+
+        val serialized = Gson().toJson("$ActualCredentialContent")
+
+
+        val CredentialContent = JsonBasedCredential(ActualCredentialContent)
+        val signedCredential = listOf(CredentialContent.sign(masterKeyPair.privateKey))
+
+        val (merkleRoot, merkleProofs) = CredentialBatches.batch(signedCredential)
+
+
+        File("result.json").writeText("$signedCredential")
+        println("""- signedCredential:. $signedCredential""")
+        println("""-----------------------------------------------""")
+        println("""- merkleRoot:. $merkleRoot""")
+
+
+
+
+
+
+
 
 
     }
